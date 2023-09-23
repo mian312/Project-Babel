fd698b7e9e3c942df3efbdea27e520aff37cdd8e
GODSTERBEN <lappy6289@gmail.com>
2023-09-23 11:28:15 +0530

    Frontend: Team image uploaded

--- Frontend/src/components/sections/Team.jsx
+++ Frontend/src/components/sections/Team.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Team = () => (
     <div className="row">
-        <div className="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up">
+        <div className="col-xl-4 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
             <div className="member">
-                <img src="../../../src/images/team/team-1.jpg" className="img-fluid" alt="" />
+                <img src="../../../src/images/team/team-6.jpg" className="img-fluid im" alt="" />
                 <div className="member-info">
                     <div className="member-info-content">
-                        <h4>Walter White</h4>
-                        <span>Chief Executive Officer</span>
+                        <h4>Prapti Mukharjee</h4>
+                        <span>Team Lead</span>
                     </div>
                     <div className="social">
                         <a href=""><i className="bi bi-twitter"></i></a>
@@ -20,13 +20,13 @@ const Team = () => (
             </div>
         </div>
 
-        <div className="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+        <div className="col-xl-4 col-lg-4 col-md-6" data-aos="fade-up">
             <div className="member">
-                <img src="../../../src/images/team/team-2.jpg" className="img-fluid" alt="" />
+                <img src="../../../src/images/team/team-1.jpg" className="img-fluid im" alt="" />
                 <div className="member-info">
                     <div className="member-info-content">
-                        <h4>Sarah Jhonson</h4>
-                        <span>Product Manager</span>
+                        <h4>Milan Dey</h4>
+                        <span>Full-Stack Developer</span>
                     </div>
                     <div className="social">
                         <a href=""><i className="bi bi-twitter"></i></a>
@@ -38,13 +38,14 @@ const Team = () => (
             </div>
         </div>
 
-        <div className="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+
+        <div className="col-xl-4 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
             <div className="member">
-                <img src="../../../src/images/team/team-3.jpg" className="img-fluid" alt="" />
+                <img src="../../../src/images/team/team-4.jpg" className="img-fluid im" alt="" />
                 <div className="member-info">
                     <div className="member-info-content">
-                        <h4>William Anderson</h4>
-                        <span>CTO</span>
+                        <h4>Debayan Kar</h4>
+                        <span>Backend Devloper</span>
                     </div>
                     <div className="social">
                         <a href=""><i className="bi bi-twitter"></i></a>
@@ -56,13 +57,13 @@ const Team = () => (
             </div>
         </div>
 
-        <div className="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+        <div className="col-xl-4 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
             <div className="member">
-                <img src="../../../src/images/team/team-4.jpg" className="img-fluid" alt="" />
+                <img src="../../../src/images/team/team-2.jpg" className="img-fluid im" alt="" />
                 <div className="member-info">
                     <div className="member-info-content">
-                        <h4>Amanda Jepson</h4>
-                        <span>Accountant</span>
+                        <h4>Pronoy Debnath</h4>
+                        <span>Frontend & Design</span>
                     </div>
                     <div className="social">
                         <a href=""><i className="bi bi-twitter"></i></a>
@@ -73,6 +74,44 @@ const Team = () => (
                 </div>
             </div>
         </div>
+
+        <div className="col-xl-4 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div className="member">
+                <img src="../../../src/images/team/team-3.jpg" className="img-fluid im" alt="" />
+                <div className="member-info">
+                    <div className="member-info-content">
+                        <h4>Subhranil Saha</h4>
+                        <span>Web Designer</span>
+                    </div>
+                    <div className="social">
+                        <a href=""><i className="bi bi-twitter"></i></a>
+                        <a href=""><i className="bi bi-facebook"></i></a>
+                        <a href=""><i className="bi bi-instagram"></i></a>
+                        <a href=""><i className="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="col-xl-4 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div className="member">
+                <img src="../../../src/images/team/team-5.jpg" className="img-fluid im" alt="" />
+                <div className="member-info">
+                    <div className="member-info-content">
+                        <h4>Basant Kumar Shaw</h4>
+                        <span>Data Manager</span>
+                    </div>
+                    <div className="social">
+                        <a href=""><i className="bi bi-twitter"></i></a>
+                        <a href=""><i className="bi bi-facebook"></i></a>
+                        <a href=""><i className="bi bi-instagram"></i></a>
+                        <a href=""><i className="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 );
 
